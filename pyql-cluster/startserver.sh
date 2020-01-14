0ecc7e02bdd7bd0854c4c0d84e4793aa02ef7bea
Codematoin <joshjamison1@gmail.com>
2020-01-14 14:48:10 +0100

    adjusting sites-available-config & startup server for host ip routing

--- pyql-cluster/startserver.sh
+++ pyql-cluster/startserver.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+HOSTIP=$(cat /etc/hosts | grep $(hostname) | awk '{print $1}')
+sed -i 's/HOSTIP/'$HOSTIP'/g' /etc/nginx/sites-enabled/sites-available-pyql-cluster
 service nginx start
 /opt/venv/bin/uwsgi --ini wsgi.ini
