1be5735e77d6234fa8f7ffb275ea1119cd77bb38
Codematoin <joshjamison1@gmail.com>
2020-02-12 16:36:04 +0100

    adjusting quorum_recovery utility to handle non-specific API requests

--- pyql-cluster/quorum_recovery.sh
+++ pyql-cluster/quorum_recovery.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-data='{"set":{"path":"'$1':80"},"where":{"uuid":"'$2'"}}'
-curl --request POST http://localhost/db/cluster/table/endpoints/update --header 'Content-type:application/json' --data $data
+data='{"set":{"path":"'$2':80"},"where":{"uuid":"'$3'"}}'
+curl --request POST http://localhost$1 --header 'Content-type:application/json' --data $data
