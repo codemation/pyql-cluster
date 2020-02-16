bd3057e6db78333467ef559ecf3562af1acb82f5
Codematoin <joshjamison1@gmail.com>
2020-02-16 23:21:10 +0100

    added checks to ensure IP address is edited inside sites-available before apache starts

--- pyql-cluster/startserver.sh
+++ pyql-cluster/startserver.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-HOSTIP=$(cat /etc/hosts | grep $(hostname) | awk '{print $1}')
-sed -i 's/HOSTIP/'$HOSTIP'/g' /etc/nginx/sites-enabled/sites-available-pyql-cluster
+while true;
+do
+    HOSTIP=$(cat /etc/hosts | grep $(hostname) | awk '{print $1}')
+    sed -i 's/HOSTIP/'$HOSTIP'/g' /etc/nginx/sites-enabled/sites-available-pyql-cluster
+    grep $HOSTIP /etc/nginx/sites-enabled/sites-available-pyql-cluster
+    if [ $? == 0 ]
+    then
+        break
+    fi
+done
 service nginx start
 /opt/venv/bin/uwsgi --ini wsgi.ini
