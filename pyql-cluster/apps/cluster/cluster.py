d74a6721ab53b3f6c627899ec55a6ceccb879581
Codematoin <joshjamison1@gmail.com>
2020-02-19 00:08:29 +0100

    correcting order of outOfQuorum endpoint state updates when detected

--- pyql-cluster/apps/cluster/cluster.py
+++ pyql-cluster/apps/cluster/cluster.py
@@ -389,11 +389,13 @@ def run(server):
             if isNodeInQuorum:
                 # need to set outQuorum endpoint tables to inSync False - so reads are not attempted from tables
                 for endpoint in outQuorum:
+                    log.warning(f"cluster_quorum - preQuorum {preQuorum}")
                     if endpoint in preQuorum['quorum']['nodes']['nodes']:
                         data['where']['uuid'] = endpoint
                         log.warning(f"cluster_quorum - marking endpoint {endpoint} tables inSync=False as endpoint is outOfQuorum")
                         server.clusters.state.update(**data['set'], where=data['where'])
                         #post_request_tables('pyql', 'state', 'update', data)
+
             quorum = server.clusters.quorum.select('*', where={'node': nodeId})[0]
             return {"message": f"quorum updated on {nodeId}", 'quorum': quorum},200
         else:
