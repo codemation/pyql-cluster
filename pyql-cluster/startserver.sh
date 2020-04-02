dc2e6906fdfe4425bc9a66f99041845d1cc178c5
Codematoin <joshjamison1@gmail.com>
2020-04-02 15:41:08 +0200

    adjusted startserver to properly use PYQL_HOST env var

--- pyql-cluster/startserver.sh
+++ pyql-cluster/startserver.sh
@@ -9,12 +9,23 @@ do
         break
     fi
 done
+
 echo $PYQL_HOST | grep $PYQL_HOST
 if [ $? -eq 0 ]
 then
-    # used by stand-alone / test pyql-cluster instances with a variable hostname 
-    sed -i 's/PYQL_HOST/'$PYQL_HOST'/g' /etc/nginx/sites-enabled/sites-available-pyql-cluster
+    while true;
+    do
+        # used by stand-alone / test pyql-cluster instances with a variable hostname 
+        sed -i 's/PYQL_HOST/'$PYQL_HOST'/g' /etc/nginx/sites-enabled/sites-available-pyql-cluster
+        grep $PYQL_HOST /etc/nginx/sites-enabled/sites-available-pyql-cluster
+        if [ $? == 0 ]
+        then
+            break
+        fi
+    done
 fi
+    
+
 
 service nginx start
 /opt/venv/bin/uwsgi --ini wsgi.ini
