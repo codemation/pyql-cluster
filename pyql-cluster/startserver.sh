56f358771dee064250b4eae7d646220e22e3cf85
Codematoin <joshjamison1@gmail.com>
2020-04-02 14:19:28 +0200

    added PYQL_HOST env check into startserver to allow stand-alone instance / testing in container

--- pyql-cluster/startserver.sh
+++ pyql-cluster/startserver.sh
@@ -9,5 +9,11 @@ do
         break
     fi
 done
+echo $PYQL_HOST | grep $PYQL_HOST
+if [ $? -eq 0 ]
+then
+    # used by stand-alone / test pyql-cluster instances with a variable hostname 
+    sed -i 's/PYQL_HOST/'$PYQL_HOST'/g' /etc/nginx/sites-enabled/sites-available-pyql-cluster
+
 service nginx start
 /opt/venv/bin/uwsgi --ini wsgi.ini
