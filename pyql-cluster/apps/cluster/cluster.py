96d001082b45678dac4d117724c69e2428cf5307
Codematoin <joshjamison1@gmail.com>
2020-05-05 00:58:48 +0200

    adjusted cluster_quorum_update to  Quorum always assume local checking node is alive

--- pyql-cluster/apps/cluster/cluster.py
+++ pyql-cluster/apps/cluster/cluster.py
@@ -448,7 +448,7 @@ def run(server):
         for endpoint in epResults:
             if epResults[endpoint]['status'] == 200:
                 inQuorumNodes.append(endpoint)
-        # Quorum always assumeds local checking node is alive
+        # Quorum always assume local checking node is alive
         if not nodeId in inQuorumNodes:
             inQuorumNodes.append(nodeId)
         inQuorum = False
