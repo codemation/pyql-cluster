bceffd8a6d90e244c901e1fe7ea1b78fdbf9b68b
Codematoin <joshjamison1@gmail.com>
2020-05-18 16:24:23 +0200

    added state_and_quorum_check decor which checks that node is inQuorum & state inSync = True to handle requests

--- pyql-cluster/apps/cluster/cluster.py
+++ pyql-cluster/apps/cluster/cluster.py
@@ -230,8 +230,9 @@ def run(server):
                 log.warning("state.inSync is False for node but this node is inQuorum = True")
                 pyqlNodes = server.clusters.endpoints.select('uuid', 'path', where={'cluster': pyql})
                 headers = dict(request.headers)
-                for h in ['Host']:
-                    headers.pop(h)
+                for node in pyqlNodes:
+                    if node['uuid'] == nodeId:
+                        headers['Host'] = node['path']
                 # using unsafe in headers to track endpoints which we have tried and should not again
                 if not 'unsafe' in headers:
                     headers['unsafe'] = nodeId
