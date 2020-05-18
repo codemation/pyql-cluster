6da50fbcef615ebfbce01832d605b80ce9803360
Codematoin <joshjamison1@gmail.com>
2020-05-18 10:45:48 +0200

    added state_and_quorum_check decor which checks that node is inQuorum & state inSync = True to handle requests

--- pyql-cluster/apps/cluster/cluster.py
+++ pyql-cluster/apps/cluster/cluster.py
@@ -206,7 +206,8 @@ def run(server):
         """
         def state_quorum_safe_func(*args, **kwargs):
             if not 'quorum' in kwargs:
-                quorum, rc = cluster_quorum(trace=trace)
+                quorum, rc = cluster_quorum()
+            kwargs['quorum'] = quorum
             if not 'quorum' in quorum or quorum['quorum']['inQuorum'] == False:
                 return {
                     "message": log.error(f"cluster pyql node {os.environ['HOSTNAME']} is not in quorum {quorum}"),
