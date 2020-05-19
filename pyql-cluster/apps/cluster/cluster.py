fd8bef28e7704348090735c0799ed2ee85088c66
Codematoin <joshjamison1@gmail.com>
2020-05-19 09:51:52 +0200

    table sync jobs & table_copy will use table_select which can benefit from accessing local db connection without probe

--- pyql-cluster/apps/cluster/cluster.py
+++ pyql-cluster/apps/cluster/cluster.py
@@ -2286,7 +2286,7 @@ def run(server):
                         #message, rc = table_cutover(clusterId, table, 'start')
                         track(f"PYQL - starting table_copy")
                         #r, rc = table_copy(cluster, table, inSyncPath, inSyncToken, inSyncUuid,  endpointPath, token, uuid, trace=kw['trace'])
-                        r, rc = table_copy(cluster, table, outOfSyncPath, outOfSyncToken, outOfSyncUuid, **kw)
+                        r, rc = table_copy(cluster, table, endpointPath, token, uuid, **kw)
                         track(f"PYQL - table_copy result: {r} rc: {rc}")
                         if rc == 500:
                             if 'not able to find an inSync endpoints' in r:
@@ -2319,7 +2319,7 @@ def run(server):
                     track(f'PYQL - end of cutover, resuming table result: {r} rc: {rc}')
                 else: 
                     #r, rc = table_copy(cluster, table, inSyncPath, inSyncToken, inSyncUuid, endpointPath, token,uuid, trace=kw['trace'])
-                    r, rc = table_copy(cluster, table, outOfSyncPath, outOfSyncToken, outOfSyncUuid, **kw)
+                    r, rc = table_copy(cluster, table, endpointPath, token, uuid, **kw)
                     track(f"table_copy results: {r} {rc}")
                     if rc == 500:
                         if 'not able to find an inSync endpoints' in r:
