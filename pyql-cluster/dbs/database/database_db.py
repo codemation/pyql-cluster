fb2c105ec1c029d0a326a7fb51a5d5f17910009a
Codematoin <joshjamison1@gmail.com>
2020-07-01 16:46:52 +0200

    converted variables to pep8_standard from camelCase

--- pyql-cluster/dbs/database/database_db.py
+++ pyql-cluster/dbs/database/database_db.py
@@ -8,30 +8,26 @@ def run(server):
         os.environ['DB_NAME'] = 'cluster' # TODO - Add to env variables config later
         if 'PYQL_TYPE' in os.environ:
             if os.environ['PYQL_TYPE'] == 'K8S' or os.environ['PYQL_TYPE'] == 'DOCKER':
-                dbName = os.getenv('DB_NAME').rstrip()
-                dbLocation = os.environ['PYQL_VOLUME_PATH']
-                config['database'] = f'{dbLocation}/{dbName}'
+                db_name = os.getenv('DB_NAME').rstrip()
+                db_location = os.environ['PYQL_VOLUME_PATH']
+                config['database'] = f'{db_location}/{db_name}'
         else:
             with open('.cmddir', 'r') as projDir:
-                for projectPath in projDir:
-                    dbName = os.getenv('DB_NAME').rstrip()
-                    config['database'] = f'{projectPath}dbs/database/{dbName}'
+                for project_path in projDir:
+                    db_name = os.getenv('DB_NAME').rstrip()
+                    config['database'] = f'{project_path}dbs/database/{db_name}'
         config['logger'] = log
         if server.PYQL_DEBUG == True:
             config['debug'] = True
-        #USE ENV PATH for PYQL library or /pyql/
-        #sys.path.append('/pyql/' if os.getenv('PYQL_PATH') == None else os.getenv('PYQL_PATH'))
-        #try:
+
         from pyql import data
         import sqlite3
         from . import setup
         log.info("finished imports")
-        server.data[dbName] = data.database(sqlite3.connect, **config)
+        server.data[db_name] = data.database(sqlite3.connect, **config)
         log.info("finished dbsetup")
         setup.attach_tables(server)
         log.info("finished attach_tables")
         return {"status": 200, "message": "database attached successfully"}, 200
-        #except Exception as e:
-        #    return {"status": 200, "message": repr(e)}, 500
     response = database_attach()
     log.info(response)
