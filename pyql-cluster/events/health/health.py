8a641b9d6848dab13ad1a20efa8825394ffd5db0
Codematoin <joshjamison1@gmail.com>
2020-07-27 12:51:49 +0200

    corrected reference to server.internal_db_check

--- pyql-cluster/events/health/health.py
+++ pyql-cluster/events/health/health.py
@@ -1,7 +1,7 @@
 async def run(server):
 
     async def server_db_check():
-        return await server.db_check()
+        return await server.internal_db_check()
     server.tasks.append(
         server_db_check
     )
