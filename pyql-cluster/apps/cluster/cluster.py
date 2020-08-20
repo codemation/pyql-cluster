c30b12d4d91d92caa103f25c165279007d29cdbe
Codematoin <joshjamison1@gmail.com>
2020-08-20 15:11:52 +0200

    corrected random endpoint select in cluster_table_copy

--- pyql-cluster/apps/cluster/cluster.py
+++ pyql-cluster/apps/cluster/cluster.py
@@ -1688,7 +1688,7 @@ async def run(server):
             method='GET',
             token=token,
             session=await get_endpoint_sessions(
-                endpoint['uuid'], 
+                endpoint_info['uuid'], 
                 **kw
             )
         )
