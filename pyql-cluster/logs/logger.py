492adc40b043aa64b157d124aea91f7079a2c761
Codematoin <joshjamison1@gmail.com>
2020-08-27 13:58:29 +0200

    checking for existing handlers before configuring

--- pyql-cluster/logs/logger.py
+++ pyql-cluster/logs/logger.py
@@ -4,31 +4,31 @@ def run(server):
 
     # Create a custom logger
     logger = logging.getLogger('pyql-cluster')
+    if not logger.handlers:
+        # Create handlers
+        i_handler = logging.StreamHandler()
+        c_handler = logging.StreamHandler()
+        f_handler = logging.FileHandler('pyql-cluster.log')
+        d_handler = logging.StreamHandler()
+        i_handler.setLevel(logging.INFO)
+        c_handler.setLevel(logging.WARNING)
+        f_handler.setLevel(logging.ERROR)
+        d_handler.setLevel(logging.DEBUG)
 
-    # Create handlers
-    i_handler = logging.StreamHandler()
-    c_handler = logging.StreamHandler()
-    f_handler = logging.FileHandler('pyql-cluster.log')
-    d_handler = logging.StreamHandler()
-    i_handler.setLevel(logging.INFO)
-    c_handler.setLevel(logging.WARNING)
-    f_handler.setLevel(logging.ERROR)
-    d_handler.setLevel(logging.DEBUG)
+        # Create formatters and add it to handlers
+        c_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+        f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        i_handler.setFormatter(f_format)
+        c_handler.setFormatter(f_format)
+        f_handler.setFormatter(f_format)
 
-    # Create formatters and add it to handlers
-    c_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
-    f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    i_handler.setFormatter(f_format)
-    c_handler.setFormatter(f_format)
-    f_handler.setFormatter(f_format)
-
-    # Add handlers to the logger
-    logger.addHandler(i_handler)
-    logger.addHandler(c_handler)
-    logger.addHandler(f_handler)
-    if server.PYQL_DEBUG == True:    
-        logger.addHandler(d_handler)
-        logger.setLevel(logging.DEBUG)
+        # Add handlers to the logger
+        logger.addHandler(i_handler)
+        logger.addHandler(c_handler)
+        logger.addHandler(f_handler)
+        if server.PYQL_DEBUG == True:    
+            logger.addHandler(d_handler)
+            logger.setLevel(logging.DEBUG)
     
     def log_and_return(log_func):
         def log(message):
