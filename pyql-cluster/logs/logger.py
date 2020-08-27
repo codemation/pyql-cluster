26d93baea108019f979f83aaddf96afdeedaaa30
Codematoin <joshjamison1@gmail.com>
2020-08-27 14:25:05 +0200

    clear existing handlers before configuring

--- pyql-cluster/logs/logger.py
+++ pyql-cluster/logs/logger.py
@@ -5,31 +5,32 @@ def run(server):
     # Create a custom logger
     logger = logging.getLogger('pyql-cluster')
     logger.propagate = False
-    if not logger.handlers:
-        # Create handlers
-        i_handler = logging.StreamHandler()
-        c_handler = logging.StreamHandler()
-        f_handler = logging.FileHandler('pyql-cluster.log')
-        d_handler = logging.StreamHandler()
-        i_handler.setLevel(logging.INFO)
-        c_handler.setLevel(logging.WARNING)
-        f_handler.setLevel(logging.ERROR)
-        d_handler.setLevel(logging.DEBUG)
+    if logger.hasHandlers()):
+        logger.handlers.clear()
+        
+    i_handler = logging.StreamHandler()
+    c_handler = logging.StreamHandler()
+    f_handler = logging.FileHandler('pyql-cluster.log')
+    d_handler = logging.StreamHandler()
+    i_handler.setLevel(logging.INFO)
+    c_handler.setLevel(logging.WARNING)
+    f_handler.setLevel(logging.ERROR)
+    d_handler.setLevel(logging.DEBUG)
 
-        # Create formatters and add it to handlers
-        c_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
-        f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        i_handler.setFormatter(f_format)
-        c_handler.setFormatter(f_format)
-        f_handler.setFormatter(f_format)
+    # Create formatters and add it to handlers
+    c_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+    f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    i_handler.setFormatter(f_format)
+    c_handler.setFormatter(f_format)
+    f_handler.setFormatter(f_format)
 
-        # Add handlers to the logger
-        logger.addHandler(i_handler)
-        logger.addHandler(c_handler)
-        logger.addHandler(f_handler)
-        if server.PYQL_DEBUG == True:    
-            logger.addHandler(d_handler)
-            logger.setLevel(logging.DEBUG)
+    # Add handlers to the logger
+    logger.addHandler(i_handler)
+    logger.addHandler(c_handler)
+    logger.addHandler(f_handler)
+    if server.PYQL_DEBUG == True:    
+        logger.addHandler(d_handler)
+        logger.setLevel(logging.DEBUG)
     
     def log_and_return(log_func):
         def log(message):
@@ -38,10 +39,10 @@ def run(server):
         return log
     
     class LogReturn:
-        def __init__(self, loggr):
-            self.debug = log_and_return(loggr.debug)
-            self.error = log_and_return(loggr.error)
-            self.warning = log_and_return(loggr.warning)
-            self.info = log_and_return(loggr.info)
-            self.exception = log_and_return(loggr.exception)
-    server.log = LogReturn(logger)
+        def __init__(self):
+            self.debug = log_and_return(logger.debug)
+            self.error = log_and_return(logger.error)
+            self.warning = log_and_return(logger.warning)
+            self.info = log_and_return(logger.info)
+            self.exception = log_and_return(logger.exception)
+    server.log = LogReturn()
