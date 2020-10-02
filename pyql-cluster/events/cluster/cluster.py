e960ed9202097fc578954e166ea105b330314b1c
Codematoin <joshjamison1@gmail.com>
2020-10-02 22:40:20 +0200

    flush workers 1 -> 2 , trimmed un-used TODO delete code / files

--- pyql-cluster/events/cluster/cluster.py
+++ pyql-cluster/events/cluster/cluster.py
@@ -19,31 +19,3 @@ async def run(server):
     server.tasks.append(
         get_factory_coro('job')
     )
-
-
-    """
-    log = server.log
-    import os
-    server.cron = {}
-    import subprocess, time, requests,json
-    print("starting job workers")
-    with open('.cmddir', 'r') as c:
-        events = f'{[l for l in c][0]}events/'
-        path = f'{events}cluster/'
-
-    # Create Job workers to pull & perform work on cluster jobs
-    log.info(f'jobworker to pull jobs from cluster job queue via /cluster/jobqueue/job')
-    #for _ in range(2):
-    #subprocess.Popen(['python', f'{path}jobworker.py', '/cluster/jobqueue/job', '10.0', events])
-    subprocess.Popen(['python', f'{path}jobworker.py', '/cluster/jobs/job/run', '10.0', events])
-
-    ## Create Table Sync Workers
-    #subprocess.Popen(['python', f'{path}tablesyncer.py', '/cluster/jobqueue/syncjob', '10.0', events])
-    # Create Table Sync Workers
-    subprocess.Popen(['python', f'{path}jobworker.py', '/cluster/jobs/syncjob/run', '10.0', events])
-
-    # Create Cron Job Workers
-    # subprocess.Popen(['python', f'{path}jobworker.py', '/cluster/jobqueue/cron', '15.0', events])
-    # Create Cron Job Workers
-    subprocess.Popen(['python', f'{path}jobworker.py', '/cluster/jobs/cron/run', '15.0', events])
-    """
