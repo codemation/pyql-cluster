14069841a75d8c4e9256367fae150394740a8164
Codematoin <joshjamison1@gmail.com>
2020-10-03 00:03:35 +0200

    corrected check for existing table before creation

--- pyql-cluster/dbs/database/tables/env.py
+++ pyql-cluster/dbs/database/tables/env.py
@@ -1,13 +1,12 @@
 async def db_attach(server):
     db = server.data['cluster']
-    if 'env' in db.tables:
-        return
-    await db.create_table(
-       'env', [
-           ('env', str, 'UNIQUE NOT NULL'), 
-           ('val', str)
-       ],
-       'env',
-       cache_enabled=True
-    )
+    if not 'env' in db.tables:
+        await db.create_table(
+        'env', [
+            ('env', str, 'UNIQUE NOT NULL'), 
+            ('val', str)
+        ],
+        'env',
+        cache_enabled=True
+        )
     server.env = db.tables['env']
