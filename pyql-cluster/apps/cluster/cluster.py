af79e33199e9035fa447c13895f6ee7bbc362a7c
Codematoin <joshjamison1@gmail.com>
2020-12-17 14:15:30 +0100

    reverted  logic for server.actions usage if ep_uuid is same as local node within write_to_txn_logs

--- pyql-cluster/apps/cluster/cluster.py
+++ pyql-cluster/apps/cluster/cluster.py
@@ -665,6 +665,7 @@ async def run(server):
             async def log_insert():
                 trace(f"starting log_insert for txn {txn}")
                 try:
+                    """
                     if ep_uuid == server.PYQL_NODE_ID:
                         trace(f"log_insert: LOCAL")
                         return {
@@ -675,6 +676,7 @@ async def run(server):
                             )
                         }
                     trace(f"log_insert: REMOTE")
+                    """
                     return {
                         ep_uuid: await server.rpc_endpoints[ep_uuid]['insert'](
                             db, 
